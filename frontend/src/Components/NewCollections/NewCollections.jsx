23ede92c88a04cd02f451c71225bfd8388090bda
Chandsi <chandsigola@gmail.com>
2024-09-15 14:27:48 +0530

    first commit

---
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -0,0 +1,28 @@
+import React, {useEffect, useState } from 'react'
+import './NewCollection.css'
+import Item from '../Item/Item'
+
+
+const NewCollections = () => {
+  const [new_collection , setNew_Collections] = useState([]);
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/newcollections')
+    .then((res) => res.json())
+    .then((data) =>setNew_Collections(data));
+
+  },[]);
+  return (
+    <div className='new-collections'>
+      <h1>NEW COLLECTIONS</h1>
+      <hr />
+      <div className="collections">
+        {new_collection.map((item ,i)=>{
+          return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default NewCollections
