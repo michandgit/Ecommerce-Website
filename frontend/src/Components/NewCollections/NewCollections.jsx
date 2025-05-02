f627ba5197bff4a8957e12eb4b3ba0c05a3d30e8
michandgit <chandsigola@gmail.com>
2025-05-02 17:37:09 +0530

    Updated

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collection , setNew_Collections] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://ecommerce-website-mn8f.onrender.com/newcollections')
     .then((res) => res.json())
     .then((data) =>setNew_Collections(data));
 
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -18,12 +18,12 @@ const ShopContextProvider = (props) =>{
 
 
     useEffect(()=>{
-        fetch('http://localhost:4000/allproducts')
+        fetch('https://ecommerce-website-mn8f.onrender.com/allproducts')
         .then((res) => res.json())
         .then((data)=>setAll_Product(data))
 
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/getcart' ,{
+            fetch('https://ecommerce-website-mn8f.onrender.com/getcart' ,{
                 method :"POST",
                 headers:{
                     Accept: "application/form-data",
@@ -40,7 +40,7 @@ const ShopContextProvider = (props) =>{
     const addToCart = (itemId) =>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/addtocart',{
+            fetch('https://ecommerce-website-mn8f.onrender.com/addtocart',{
                 method:"POST",
                 headers:{
                     Accept: 'application/form-data',
@@ -56,7 +56,7 @@ const ShopContextProvider = (props) =>{
     const removeFromCart = (itemId) =>{
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
         if(localStorage.getItem('auth-token')){
-            fetch('http://localhost:4000/removefromcart',{
+            fetch('https://ecommerce-website-mn8f.onrender.com/removefromcart',{
                 method:"POST",
                 headers:{
                     Accept: 'application/form-data',
